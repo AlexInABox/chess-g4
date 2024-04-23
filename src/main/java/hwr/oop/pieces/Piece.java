a7d8e122fcae59271e68415334fca0a31f015175
AlexInABox <main@alexinabox.de>
2024-04-23 18:35:22 +0200

    no usages

--- src/main/java/hwr/oop/pieces/Piece.java
+++ src/main/java/hwr/oop/pieces/Piece.java
@@ -231,11 +231,6 @@ public class Piece {
   }
 
   @Override
-  public String toString() {
-    return "Piece{type=" + type + ", color=" + color + ", position=" + position + ", symbol=" + symbol + '}';
-  }
-
-  @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
