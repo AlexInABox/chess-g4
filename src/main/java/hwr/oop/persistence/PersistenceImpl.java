cf7f98324f09e22173fd7d452248d79e205b8cc8
laura194 <voss.2000@gmx.de>
2024-04-28 10:41:15 +0200

    Create PersistenceImpl.java

---
+++ src/main/java/hwr/oop/persistence/PersistenceImpl.java
@@ -0,0 +1,27 @@
+package hwr.oop.persistence;
+
+import java.io.*;
+
+public class PersistenceImpl implements Persistence{
+
+  @Override
+  public void write(Object object, String filePath) {
+
+    try(FileOutputStream f = new FileOutputStream((filePath))){
+      ObjectOutputStream o = new ObjectOutputStream(f);
+      o.writeObject(object);
+    } catch (IOException e) {
+      throw new PersistenceException("Cannot write.");
+    }
+  }
+
+  @Override
+  public Object read(String filePath) {
+    try(FileInputStream f = new FileInputStream(filePath)){
+      ObjectInputStream o = new ObjectInputStream(f);
+      return o.readObject();
+    } catch (IOException | ClassNotFoundException e) {
+      throw new PersistenceException("Cannot read.");
+    }
+  }
+}
