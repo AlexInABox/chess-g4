7200b82279fd9c41f8c1706d5b9e6579366486e9
laura194 <voss.2000@gmx.de>
2024-05-13 17:26:34 +0200

    - Change file name of File Based Persistence adapter - change read() and write() to load() and save()

--- src/main/java/hwr/oop/persistence/Persistence.java
+++ src/main/java/hwr/oop/persistence/Persistence.java
@@ -3,7 +3,7 @@ package hwr.oop.persistence;
 import java.nio.file.Path;
 
 public interface Persistence {
-  void write(Object object, Path filePath);
+  void save(Object object, Path filePath);
 
-  Object read(Path filePath);
+  Object load(Path filePath);
 }
