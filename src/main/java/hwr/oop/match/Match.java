b319141ca4f8d9b2e5240ddb1b7d497c1b8feba6
laura194 <voss.2000@gmx.de>
2024-05-21 18:24:26 +0200

    Match: Generate toString(), equals(), hashCode() + Tests

--- src/main/java/hwr/oop/match/Match.java
+++ src/main/java/hwr/oop/match/Match.java
@@ -5,6 +5,7 @@ import hwr.oop.board.ChessBoard;
 import hwr.oop.board.ChessBoardException;
 import hwr.oop.player.Player;
 import java.io.Serializable;
+import java.util.Objects;
 
 public class Match implements Serializable {
   private final Player playerWhite;
@@ -47,5 +48,31 @@ public class Match implements Serializable {
 
   public String getFEN() {
     return fenNotation;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Match match = (Match) o;
+    return moveCount == match.moveCount && gameEnded == match.gameEnded && Objects.equals(playerWhite, match.playerWhite) && Objects.equals(playerBlack, match.playerBlack) && Objects.equals(board, match.board) && Objects.equals(fenNotation, match.fenNotation) && nextToMove == match.nextToMove;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(playerWhite, playerBlack, board, fenNotation, nextToMove, moveCount, gameEnded);
+  }
+
+  @Override
+  public String toString() {
+    return "Match{" +
+            "playerWhite=" + playerWhite +
+            ", playerBlack=" + playerBlack +
+            ", board=" + board +
+            ", fenNotation='" + fenNotation + '\'' +
+            ", nextToMove=" + nextToMove +
+            ", moveCount=" + moveCount +
+            ", gameEnded=" + gameEnded +
+            '}';
   }
 }
--- src/test/java/hwr/oop/match/MatchTest.java
+++ src/test/java/hwr/oop/match/MatchTest.java
@@ -3,10 +3,11 @@ package hwr.oop.match;
 import hwr.oop.board.ChessBoard;
 import hwr.oop.board.ChessBoardException;
 import hwr.oop.player.Player;
-import hwr.oop.Position;
 
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 class MatchTest {
   private Match match;
@@ -51,5 +52,87 @@ class MatchTest {
           softly.assertThat(match.getPlayerBlack()).isEqualTo(blackPlayer);
           softly.assertThat(match.getFEN()).isEqualTo(fenNotation);
         });
+  }
+  @Test
+  void testEquals_IdenticalInstances() throws ChessBoardException {
+    Player playerWhite = new Player("White");
+    Player playerBlack = new Player("Black");
+    ChessBoard board = new ChessBoard();
+    Match match1 = new Match(playerWhite, playerBlack, board);
+    Match match2 = new Match(playerWhite, playerBlack, board);
+    assertThat(match1.equals(match2)).isTrue();
+  }
+
+  @SuppressWarnings("EqualsWithItself")
+  @Test
+  void testEquals_SameInstance() throws ChessBoardException {
+    Player playerWhite = new Player("White");
+    Player playerBlack = new Player("Black");
+    ChessBoard board = new ChessBoard();
+    Match match = new Match(playerWhite, playerBlack, board);
+    assertThat(match.equals(match)).isTrue();
+  }
+
+  @SuppressWarnings("ConstantConditions")
+  @Test
+  void testEquals_InstanceNull() throws ChessBoardException {
+    Player playerWhite = new Player("White");
+    Player playerBlack = new Player("Black");
+    ChessBoard board = new ChessBoard();
+    Match match = new Match(playerWhite, playerBlack, board);
+    assertThat(match.equals(null)).isFalse();
+  }
+
+  @SuppressWarnings("EqualsBetweenInconvertibleTypes")
+  @Test
+  void testEquals_DifferentClass() throws ChessBoardException {
+    Player playerWhite = new Player("White");
+    Player playerBlack = new Player("Black");
+    ChessBoard board = new ChessBoard();
+    Match match = new Match(playerWhite, playerBlack, board);
+    assertThat(match.equals(board)).isFalse();
+  }
+
+  @Test
+  void testHashCode_IdenticalHashCode() throws ChessBoardException {
+    Player playerWhite = new Player("White");
+    Player playerBlack = new Player("Black");
+    ChessBoard board = new ChessBoard();
+    Match match1 = new Match(playerWhite, playerBlack, board);
+    Match match2 = new Match(playerWhite, playerBlack, board);
+    assertThat(match1.hashCode()).isEqualTo(match2.hashCode());
+  }
+
+  @Test
+  void testHashCode_DifferentHashCode() throws ChessBoardException {
+    Player playerWhite1 = new Player("White");
+    Player playerBlack1 = new Player("Black");
+    ChessBoard board1 = new ChessBoard();
+    Match match1 = new Match(playerWhite1, playerBlack1, board1);
+
+    Player playerWhite2 = new Player("Alice");
+    Player playerBlack2 = new Player("James");
+    ChessBoard board2 = new ChessBoard();
+    Match match2 = new Match(playerWhite2, playerBlack2, board2);
+
+    assertThat(match1.hashCode()).isNotEqualTo(match2.hashCode());
+  }
+
+  @Test
+  void testToString() throws ChessBoardException {
+    Player playerWhite = new Player("White");
+    Player playerBlack = new Player("Black");
+    ChessBoard board = new ChessBoard();
+    Match match = new Match(playerWhite, playerBlack, board);
+    String expectedString = "Match{" +
+            "playerWhite=" + playerWhite +
+            ", playerBlack=" + playerBlack +
+            ", board=" + board +
+            ", fenNotation='" + null + '\'' +
+            ", nextToMove=WHITE" +
+            ", moveCount=0" +
+            ", gameEnded=false" +
+            '}';
+    assertThat(match.toString()).isEqualTo(expectedString);
   }
 }
