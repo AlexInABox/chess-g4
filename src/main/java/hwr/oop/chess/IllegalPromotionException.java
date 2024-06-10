1fdca80fb8c5113ce6de1909880128e3a112ef22
Alexander Betke <s_betke23@stud.hwr-berlin.de>
2024-06-10 10:22:37 +0200

    add promotion endpoint to ChessBoard

--- src/main/java/hwr/oop/chess/IllegalPromotionException.java
+++ src/main/java/hwr/oop/chess/IllegalPromotionException.java
@@ -1,7 +1,7 @@
 package hwr.oop.chess;
 
-public class IllegalPromotionException extends RuntimeException{
-  public IllegalPromotionException() {
-    super("Promotion is not allowed: The specified type is invalid. Valid promotion types are 'Queen', 'Rook', 'Bishop', or 'Knight'.");
+public class IllegalPromotionException extends RuntimeException {
+  public IllegalPromotionException(String message) {
+    super(message);
   }
 }
